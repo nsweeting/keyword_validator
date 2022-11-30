9c65b963be5554335228eee1c49b1730c939ed3f
Nick Sweeting <nsweeting@gmail.com>
2022-11-30 09:03:31 -0500

    Remove change

--- lib/keyword_validator.ex
+++ lib/keyword_validator.ex
@@ -398,7 +398,7 @@ defmodule KeywordValidator do
   defp validate_is(:fun, _val), do: {:error, "must be a function"}
 
   defp validate_is({:fun, arity}, val) when is_function(val, arity),
-    do: {:ok, {{:fun, arity}, val}}
+    do: {:ok, val}
 
   defp validate_is({:fun, arity}, _val),
     do: {:error, "must be a function of arity #{arity}"}
